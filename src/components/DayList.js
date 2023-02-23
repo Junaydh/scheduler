3129ab8188308ec6d48974bdf473bd859a47659f
Junayd H <junaydhus@gmail.com>
2023-02-23 10:30:43 -0800

    fixed prop names to fix storybook bug

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,7 +4,7 @@ import DayListItem from "components/DayListItem";
 export default function DayList(props) {
   const days = props.days.map(day => {
     return (
-      <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.value} setDay={props.props.onChange} />
+      <DayListItem key={day.id} name={day.name} spots={day.spots} selected={day.name === props.value} setDay={props.onChange} />
     );
   });
   return (
