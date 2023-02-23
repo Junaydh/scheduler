ae510268ed472500f2ef29ba49b35e6bd3fe4376
Junayd H <junaydhus@gmail.com>
2023-02-23 14:41:27 -0800

    add conditional rendering for show and empty views with required props

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {Fragment} from "react";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 import "components/Appointment/styles.scss";
 
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time}/>
+      {props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer}/>}
+      {!props.interview && <Empty />}
+    </article>
   )
 };
