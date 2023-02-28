713200cb19ba930a5825db5c7c167e1bf042854f
Junayd H <junaydhus@gmail.com>
2023-02-27 18:31:52 -0800

    replace multiple states with single state object and add functions to update it

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,8 +46,11 @@ const appointments = {
 
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []});
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
   const appointmentsArr = Object.values(appointments).map(appointment => {
     return (
       <Appointment key={appointment.id} {...appointment} />
@@ -65,8 +68,8 @@ export default function Application(props) {
         <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler"/>
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} 
-          value={day} 
+          <DayList days={state.days} 
+          value={state.day} 
           onChange={setDay} 
         />
         </nav>
