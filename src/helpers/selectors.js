20c5a4fbffdc76fca0017396570d68161b329918
Junayd H <junaydhus@gmail.com>
2023-02-28 13:47:25 -0800

    implement getinterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,3 +9,19 @@ export function getAppointmentsForDay(state, day) {
   }
   return result;
 }
+
+export function getInterview(state, interview) {
+  if (interview != null) {
+    const result = {
+      student: interview.student,
+      interviewer: {
+        id: interview.interviewer,
+        name: state.interviewers[interview.interviewer].name,
+        avatar: state.interviewers[interview.interviewer].avatar
+      }
+    };
+    return result;
+  } else {
+    return null;
+  };
+}
