68ae4e120b01158648ef09c3965ae8d464090440
Junayd H <junaydhus@gmail.com>
2023-02-28 17:34:57 -0800

    added transition function when create button is clicked

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -9,6 +9,7 @@ import useVisualMode from "hooks/useVisualMode";
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
+  const CREATE = 'CREATE';
 
   const {mode, transition, back} = useVisualMode(
     props.interview ? SHOW : EMPTY
@@ -17,7 +18,7 @@ export default function Appointment(props) {
     <article className="appointment">
       <Header time={props.time}/>
       {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer}/>}
-      {mode === EMPTY && <Empty onAdd={() => console.log('clicked onAdd')}/>}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>}
     </article>
   )
 };
