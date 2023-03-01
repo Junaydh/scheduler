bcaec56ce7958acd98e307c6be39b2d5e0350e88
Junayd H <junaydhus@gmail.com>
2023-02-28 17:45:39 -0800

    add logic for form view and onCancel function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -20,7 +20,7 @@ export default function Appointment(props) {
       <Header time={props.time}/>
       {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer}/>}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)}/>}
-      {mode === CREATE && <Form interviewers={[]}/>}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()}/>}
     </article>
   )
 };
