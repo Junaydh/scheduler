9fa3c348dddb6058a204948e8a7909c88100d5e0
Junayd H <junaydhus@gmail.com>
2023-02-28 16:43:05 -0800

    add test for back function of custom hook

--- src/hooks/__tests__/useVisualMode.test.js
+++ src/hooks/__tests__/useVisualMode.test.js
@@ -4,6 +4,7 @@ import useVisualMode from "hooks/useVisualMode";
 
 const FIRST = "FIRST";
 const SECOND = 'SECOND';
+const THIRD = 'THIRD';
 
 test("useVisualMode should initialize with default value", () => {
   const { result } = renderHook(() => useVisualMode(FIRST));
@@ -12,8 +13,24 @@ test("useVisualMode should initialize with default value", () => {
 });
 
 test('useVisualMode should transition to another mode', () => {
-  const {result} = renderHook(() => useVisualMode(SECOND));
+  const {result} = renderHook(() => useVisualMode(FIRST));
 
   act(() => result.current.transition(SECOND));
   expect(result.current.mode).toBe(SECOND);
+})
+
+test('useVisualMode should return to previous mode', () => {
+  const {result} = renderHook(() => useVisualMode(FIRST));
+
+  act(() => result.current.transition(SECOND));
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.transition(THIRD));
+  expect(result.current.mode).toBe(THIRD);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(SECOND);
+
+  act(() => result.current.back());
+  expect(result.current.mode).toBe(FIRST);
 })
