b5317fb4089df1988321e5d972c995a4a08d39e0
Junayd H <junaydhus@gmail.com>
2023-02-28 17:25:53 -0800

    add transition with replace functionality

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,9 +4,15 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = function(newMode) {
-    setMode(newMode);
-    setHistory([...history, newMode]);
+  const transition = function(newMode, replace = false) {
+    if (replace) {
+      history.pop();
+      setMode(newMode);
+      setHistory([...history, newMode]);
+    } else {
+      setMode(newMode);
+      setHistory([...history, newMode]);
+    }
   };
 
   const back = function() {
@@ -15,7 +21,7 @@ export default function useVisualMode(initial) {
       setMode(history[history.length - 1]);
     }
   };
-  
+
   return {
     mode,
     transition,
