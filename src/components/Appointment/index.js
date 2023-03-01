818fb54aaedc42f4c24b592258be0893f858629a
Junayd H <junaydhus@gmail.com>
2023-02-28 17:34:29 -0800

    add custom hook to appointment component for default views

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,21 @@ import Header from "./Header";
 import Empty from "./Empty";
 import Show from "./Show";
 import "components/Appointment/styles.scss";
+import useVisualMode from "hooks/useVisualMode";
 
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
 
+  const {mode, transition, back} = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  )
   return (
     <article className="appointment">
       <Header time={props.time}/>
-      {props.interview && <Show student={props.interview.student} interviewer={props.interview.interviewer}/>}
-      {!props.interview && <Empty />}
+      {mode === SHOW && <Show student={props.interview.student} interviewer={props.interview.interviewer}/>}
+      {mode === EMPTY && <Empty onAdd={() => console.log('clicked onAdd')}/>}
     </article>
   )
 };
