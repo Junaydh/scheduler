579e4526095af2251983a9aa0dd0088fde997f6c
Junayd H <junaydhus@gmail.com>
2023-03-01 17:44:43 -0800

    remove unused hooks

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 import "components/Application.scss";
 import DayList from "components/DayList";
