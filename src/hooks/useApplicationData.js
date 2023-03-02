9f5fd83e01c3203d6b111c2ecc60499777f611de
Junayd H <junaydhus@gmail.com>
2023-03-01 17:42:59 -0800

    add spots update logic to cancel and book interview

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -19,7 +19,17 @@ export default function useApplicationData(initial) {
     };
     return Axios.put(`/api/appointments/${id}`, {interview})
     .then(() => {
-      setState({...state, appointments})
+      setState({
+        ...state, 
+        appointments,
+        days: state.days.map(day => {
+          if (day.name === state.day) {
+            return {...day, spots: day.spots - 1}
+          } else {
+            return day
+          }
+        })
+      })
     })
   }
   function cancelInterview(id) {
@@ -33,9 +43,20 @@ export default function useApplicationData(initial) {
     };
     return Axios.delete(`/api/appointments/${id}`)
     .then(() => {
-      setState({...state, appointments})
-    })
+      setState({
+        ...state, 
+        appointments, 
+        days: state.days.map(day => {
+          if (day.name === state.day) {
+            return {...day, spots: day.spots + 1}
+          } else {
+            return day
+          }
+        })
+      })
+   })
   }
+
   const setDay = day => setState({ ...state, day });
   useEffect(() => {
     Promise.all([
