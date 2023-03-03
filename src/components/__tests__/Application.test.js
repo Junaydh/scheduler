84414ddc9eb21e87c971d07586f9a49b0f225a8a
Junayd H <junaydhus@gmail.com>
2023-03-02 16:21:24 -0800

    implement cancel interview integration test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -43,3 +43,31 @@ it("loads data, books an interview and reduces the spots remaining for the first
 
   expect(getByText(day, /no spots remaining/i)).toBeInTheDocument();
 });
+
+it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+  // 1. Render the Application.
+  const { container, debug } = render(<Application />);
+
+  // 2. Wait until the text "Archie Cohen" is displayed.
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  const appointments = getAllByTestId(container, "appointment");
+
+  const appointment = appointments[1];
+
+  fireEvent.click(getByAltText(appointment, "Delete"));
+
+  expect(getByText(appointment, /are you sure you would like to delete\?/i)).toBeInTheDocument();
+
+  fireEvent.click(getByText(appointment, "Confirm"))
+
+  expect(getByText(appointment, /deleting/i)).toBeInTheDocument();
+
+  await waitForElementToBeRemoved(() => getByText(appointment, /deleting/i));
+
+  const day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+
+  expect(getByText(day, /2 spots remaining/i)).toBeInTheDocument();
+});
