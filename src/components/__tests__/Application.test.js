66c4d4931551920e39059c2b8e32423ea23122ab
Junayd H <junaydhus@gmail.com>
2023-03-02 16:59:48 -0800

    plan and create edit appointment integration test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getByAltText, getByPlaceholderText, waitForElementToBeRemoved, queryByText, getAllByTestId} from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, prettyDOM, getByText, getByAltText, getByPlaceholderText, waitForElementToBeRemoved, queryByText, getAllByTestId, getByDisplayValue} from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -55,19 +55,65 @@ it("loads data, cancels an interview and increases the spots remaining for Monda
 
   const appointment = appointments[1];
 
+  // 3. click on delete button
   fireEvent.click(getByAltText(appointment, "Delete"));
 
+  // 4. check that confirmation component is shown
   expect(getByText(appointment, /are you sure you would like to delete\?/i)).toBeInTheDocument();
 
+  // 5. click on confirm button
   fireEvent.click(getByText(appointment, "Confirm"))
 
+  // 6. check that deleting satus message shows
   expect(getByText(appointment, /deleting/i)).toBeInTheDocument();
 
+  // 7. wait for status message to be removed
   await waitForElementToBeRemoved(() => getByText(appointment, /deleting/i));
 
+  // 8. check that spots remaining is 2 for monday daylistitem
   const day = getAllByTestId(container, "day").find(day =>
     queryByText(day, "Monday")
   );
 
   expect(getByText(day, /2 spots remaining/i)).toBeInTheDocument();
 });
+
+it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+  const { container, debug } = render(<Application />);
+
+  // 2. Wait until the text "Archie Cohen" is displayed.
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  const appointments = getAllByTestId(container, "appointment");
+
+  const appointment = appointments[1];
+
+  debug();
+
+  // 3. click on edit button
+  fireEvent.click(getByAltText(appointment, "Edit"));
+
+  // 4. change student name and interviewer
+  fireEvent.change(getByDisplayValue(appointment, "Archie Cohen"), {target: {value: "New Name"}});
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+
+  // 5. click save button
+  fireEvent.click(getByText(appointment, "Save"));
+
+  // 6. check that saving status message is displayed
+  expect(getByText(appointment, /saving/i)).toBeInTheDocument();
+
+  // 7. wait for status component to be removed
+  await waitForElementToBeRemoved(() => getByText(appointment, /saving/i));
+
+  // 8. check that student name and interviewer has changed in show component
+  expect(getByText(appointment, "New Name")).toBeInTheDocument();
+  expect(getByText(appointment, "Sylvia Palmer")).toBeInTheDocument();
+
+  // 9. check that spots remaining has stayed the same for the monday daylistitem
+  const day = getAllByTestId(container, "day").find(day =>
+    queryByText(day, "Monday")
+  );
+
+  expect(getByText(day, /1 spot remaining/i)).toBeInTheDocument();
+})
